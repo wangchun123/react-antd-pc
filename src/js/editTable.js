e478115ea10933e267544a541c5c3db3788f70b1
chunw <chunw@chunwdeMacBook-Pro.local>
2020-02-12 23:15:28 +0800

    collapseTable

--- src/js/editTable.js
+++ src/js/editTable.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Table } from "@alifd/next";
+import { cloneDeep } from "lodash";
 
 const dataSource = () => {
   const result = [];
@@ -8,6 +9,7 @@ const dataSource = () => {
       title: {
         name: `Quotation for 1PCS Nano ${3 + i}.0 controller compatible`
       },
+      key: 100306660940 + i,
       id: 100306660940 + i,
       time: 2000 + i
     });
@@ -16,19 +18,54 @@ const dataSource = () => {
 };
 
 const EditTable = () => {
-  const handelEdit = record => {
-    console.log(record);
+  const [openRowKeys, setOpenRowKeys] = useState([]);
+  const [data, setData] = useState();
+
+  const handelEdit = (value, index, record) => {
+    const { id, showButton } = record;
+    const newData = cloneDeep(data);
+    const newOpenRowKeys = cloneDeep(openRowKeys);
+    
+    if (newData[index / 2].showButton) {
+      //收起状态
+      newOpenRowKeys.splice(openRowKeys.indexOf(id), 1);
+    } else {
+      //展开状态
+      newOpenRowKeys.push(id);
+    }
+
+    setOpenRowKeys(newOpenRowKeys);
+
+    //控制button文案；
+    newData[index / 2].showButton = !showButton;
+    setData(newData);
   };
 
+  const fetchData = () => {
+    const data = dataSource().map(item => ({ ...item, showButton: false }));
+    setTimeout(() => {
+      setData(data);
+    }, 200);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <Table dataSource={dataSource()}>
+    <Table
+      dataSource={data}
+      openRowKeys={openRowKeys}
+      hasExpandedRowCtrl={false}
+      expandedRowRender={() => "123"}
+    >
       <Table.Column title="Id" dataIndex="id" />
       <Table.Column title="Title" dataIndex="title.name" />
       <Table.Column title="Time" dataIndex="time" />
       <Table.Column
         cell={(value, index, record) => (
-          <a href="javascript:;" onClick={handelEdit(record)}>
-            Remove({record.id})
+          <a onClick={() => handelEdit(value, index, record)}>
+            {!record.showButton ? "展开" : "收起"}
           </a>
         )}
       />
@@ -37,7 +74,3 @@ const EditTable = () => {
 };
 
 export default EditTable;
-// const render = (value, index, record) => {
-//     return ;
-// };
-// ReactDOM.render(, mountNode);
