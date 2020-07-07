c0817d805747d3c0cc19fa947e26e1c8294438b5
qw <123@163.com>
2020-07-07 15:48:30 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./css/index.css";
-import "./css/antd-min.css";
-import "@alifd/next/dist/next.css";
-import Index from "./components/Global";
-import registerServiceWorker from "./registerServiceWorker";
-import { HashRouter, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import './css/antd-min.css';
+import '@alifd/next/dist/next.css';
+import Index from './components/Global';
+import registerServiceWorker from './registerServiceWorker';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
 
 const Routers = () => {
   return (
-    <HashRouter>
-      <div>
-        <Route
-          path="/"
-          exact={true}
-          component={() => <Redirect to="/index" />}
-        />
-        <Route path="/index" component={Index} />
-      </div>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <div>
+          <Route
+            path="/"
+            exact={true}
+            component={() => <Redirect to="/index" />}
+          />
+          <Route path="/index" component={Index} />
+        </div>
+      </HashRouter>
+    </Provider>
   );
 };
 
-ReactDOM.render(<Routers />, document.getElementById("root"));
+ReactDOM.render(<Routers />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/redux/reducers.js
@@ -0,0 +1,19 @@
+import action from './actions';
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: [],
+};
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter,
+      });
+    default:
+      return state;
+  }
+}
+
+export default todoApp;
